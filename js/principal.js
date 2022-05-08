f98766ba186538bf3611b44c7165c2c961e83868
nely de faria <nelly.nf54@gmail.com>
2022-05-08 17:16:47 -0300

    Validando dados antes de realizar o cálculo do IMC

--- js/principal.js
+++ js/principal.js
@@ -5,7 +5,25 @@ let tdAltura = paciente.querySelector('.info-altura');
 let peso = tdPeso.textContent;
 let altura = tdAltura.textContent;
 
-let imc = peso / (altura * altura);
-tdImc = document.querySelector('.info-imc');
-tdImc.textContent = imc;
+let tdImc = document.querySelector('.info-imc');
+
+let pesoEhValido = true;
+let alturaEhValida = true;
+
+if ((peso <= 0) || (peso >= 1000)) {
+    console.log('Peso inválido!');
+    pesoEhValido = false;
+    tdImc.textContent = 'Peso inválido';
+} 
+
+if ((altura <= 0) || (altura >= 3.00)) {
+    console.log('Altura inválida!');
+    alturaEhValida = false;
+    tdImc.textContent = 'Altura inválida';
+}
+
+if (pesoEhValido && alturaEhValida) {
+    let imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
 
