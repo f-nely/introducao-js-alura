43d755839acf8889461048f488d0e063a1b7aa61
nely de faria <nelly.nf54@gmail.com>
2022-05-08 16:43:57 -0300

    Calculando e exibindo o IMC

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,11 @@
-let titulo = document.querySelector('.titulo');
-titulo.textContent = 'Aparecida Nutricionista';
+let paciente = document.querySelector('#primeiro-paciente');
+let tdPeso = paciente.querySelector('.info-peso');
+let tdAltura = paciente.querySelector('.info-altura');
+
+let peso = tdPeso.textContent;
+let altura = tdAltura.textContent;
+
+let imc = peso / (altura * altura);
+tdImc = document.querySelector('.info-imc');
+tdImc.textContent = imc;
+
