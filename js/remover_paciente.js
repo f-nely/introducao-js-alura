7576ffeabfe2c452c6979ccd50efe9b4e3982a2d
nely de faria <nelly.nf54@gmail.com>
2022-05-15 12:52:50 -0300

    Removendo pacientes com animação

--- js/remover_paciente.js
+++ js/remover_paciente.js
@@ -1,9 +1,18 @@
 let TrPacientes = document.querySelectorAll('.paciente');
-console.log(TrPacientes);
 
-TrPacientes.forEach(function(paciente) {
+let tabela = document.querySelector('table');
+
+tabela.addEventListener('dblclick', function(event) {
+    event.target.parentNode.classList.add('fade-out');
+
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500); 
+});
+
+/* TrPacientes.forEach(function(paciente) {
     paciente.addEventListener('dblclick', function() {
         console.log('Fui clicado com duplo clique!');
         this.remove();
     });
-});
+}); */
