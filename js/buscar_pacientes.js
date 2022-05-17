40438909c7be27b44c9615cd20f937378c4638fb
nely de faria <nelly.nf54@gmail.com>
2022-05-17 13:02:01 -0300

    Adicionando pacientes na tabela via ajax

--- js/buscar_pacientes.js
+++ js/buscar_pacientes.js
@@ -1,5 +1,21 @@
 let buscarPacientes = document.querySelector('#buscar-pacientes');
 
 buscarPacientes.addEventListener('click', function() {
-    console.log('Buscando pacientes...')
+    console.log('Buscando pacientes...');
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes');
+
+    xhr.addEventListener('load', function() {
+        let resposta = xhr.responseText;
+
+        let pacientes = JSON.parse(resposta);
+        
+        pacientes.forEach(function (paciente) {
+            adicionaPacienteTabela(paciente);
+        });
+    });
+
+    xhr.send();
 });
